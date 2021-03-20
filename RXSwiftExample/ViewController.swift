a04b45f3ec5587e1f87c0ef4435b6b52e36bb92f
Silas S. Caxias <scaxias@luxfacta.com>
2021-03-20 17:55:34 -0300

    Initial Commit

---
+++ RXSwiftExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RXSwiftExample
+//
+//  Created by Silas S. Caxias on 20/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
